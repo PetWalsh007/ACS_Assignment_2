eb3b75983fab92128a29e298de9b305bbb7b15e4
Peter Walsh <peterwalsh0701@gmail.com>
2025-04-15 23:06:58 +0100

    defining target group

---
+++ target_group.tf
@@ -0,0 +1,24 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group.html
+
+# 
+
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "assign2-web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main_vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "assign2-web-tg"
+  }
+}
