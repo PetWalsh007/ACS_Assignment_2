0ec040e9e3ce8f41fbaa10452334d79c4e3bcaff
Peter Walsh <peterwalsh0701@gmail.com>
2025-04-15 23:57:29 +0100

    Fixed aws region missing from vars

--- variables.tf
+++ variables.tf
@@ -18,3 +18,9 @@ TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metad
 curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id >> /var/www/html/id.html
 EOT
 }
+
+
+variable "aws_region" {
+  description = "AWS region that we will work in"
+  default     = "us-east-1"
+}
