3fb87827f37b456bc9f3ff636bef4578fec2aa6e
Peter Walsh <peterwalsh0701@gmail.com>
2025-04-15 23:42:00 +0100

    Fix for missing .ARN

--- alb.tf
+++ alb.tf
@@ -18,12 +18,12 @@ resource "aws_lb" "web_alb" {
 }
 
 resource "aws_lb_listener" "web_alb_listener" {
-  load_balancer_arn = aws_lb.web_alb
+  load_balancer_arn = aws_lb.web_alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.web_tg
+    target_group_arn = aws_lb_target_group.web_tg.arn
   }
 }
