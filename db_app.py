42155ff00319ec3aa32a730335b137ce23b23d47
Peter Walsh <peterwalsh0701@gmail.com>
2025-04-21 21:36:20 +0100

    Fix for errors after re adding debug to app

--- db_app.py
+++ db_app.py
@@ -62,5 +62,5 @@ def get_users():
 
 # specify port 5000 - what we have open in security group
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8050, debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
 
