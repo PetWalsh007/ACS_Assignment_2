68c15c60c9f51e02bedc03e4850734e913e76abd
Peter Walsh <peterwalsh0701@gmail.com>
2025-04-21 22:35:24 +0100

    V5 launch tem AMI id

--- launch_template.tf
+++ launch_template.tf
@@ -3,7 +3,7 @@
 
 resource "aws_launch_template" "web_side_lt" {
   name_prefix   = "assign2-web-lt"
-  image_id = "ami-0e8863d98d3e7d2ed" # custom server AMI id 
+  image_id = "ami-0b69857ba18d9090b" # custom server AMI id 
   instance_type = "t2.nano"
   key_name      = var.keypair
   vpc_security_group_ids = [aws_security_group.web_sg.id]
