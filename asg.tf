dd549540cbd0c7dd2b60399e18c622e42c55a570
Peter Walsh <peterwalsh0701@gmail.com>
2025-04-23 21:35:03 +0100

    Added 2nd AZ to ASG

--- asg.tf
+++ asg.tf
@@ -3,10 +3,7 @@ resource "aws_autoscaling_group" "web_asg" {
   min_size                  = 2
   max_size                  = 5
   desired_capacity          = 2
-    vpc_zone_identifier       = [
-    aws_subnet.webserver_subnet_az1.id,
-    aws_subnet.webserver_subnet_az2.id
-  ]
+  vpc_zone_identifier       = [aws_subnet.webserver_subnet_az1.id, aws_subnet.webserver_subnet_az2.id]
   health_check_type         = "ELB"
   health_check_grace_period = 30   # defualt is 300 via docs - shortened to 30
   target_group_arns         = [aws_lb_target_group.web_tg.arn]
